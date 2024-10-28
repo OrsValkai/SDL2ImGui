4d70251c1c44ebbc5cee6a02ad62a082dbf77759
Ors <ors_valkai@yahoo.com>
2024-10-28 12:18:59 +0200

    * work with potential target instead, so animations aren't interrupted for same direction move;

--- src/AIControl.cpp
+++ src/AIControl.cpp
@@ -13,20 +13,20 @@ AIControl::AIControl(PlayGround& playGround, unsigned short startTileId)
 void AIControl::Update(float deltaTime, vo::IDrawable* pDrawable) {
 	static std::default_random_engine randEng;
 
-	if (auto& targetTileId = GetTargetId(); GetCurrentId() == targetTileId) {
+	if (const auto& targetTileId = GetTargetId(); GetPotentialTargetId() != targetTileId) {
 		std::array<unsigned short, 4> neighbs;
 		auto nCount = m_playGround.GetNeighborIdsForTileAt(neighbs, targetTileId);
         std::uniform_int_distribution<unsigned short> randDist(0, 6);
 
 		for (unsigned short i = 0; i < nCount; i++) {
-			if (neighbs[i] != m_prevId) {
-				const auto& nTile = m_playGround.GetTileAt(neighbs[i]);
+			if (neighbs[i] == m_prevId)
+				continue;
 
-				if ((nTile.HasFlagAny(TileEntry::Flags::Destroyable) || !nTile.HasFlagAny(TileEntry::Flags::Occupied)) && randDist(randEng) >= 3) {
-					targetTileId = neighbs[i];
-					m_prevId = targetTileId;
-					break;
-				}
+			const auto& nTile = m_playGround.GetTileAt(neighbs[i]);
+			if ((nTile.HasFlagAny(TileEntry::Flags::Destroyable) || !nTile.HasFlagAny(TileEntry::Flags::Occupied)) && randDist(randEng) >= 3) {
+				GetPotentialTargetId() = neighbs[i];
+				m_prevId = neighbs[i];
+				break;
 			}
 		}
     }
