6ba1c77f1ac46f56cdc9d88f5ace8d46947857d1
Örs Valkai <ors_valkai@yahoo.com>
2024-11-02 23:42:14 +0200

    * BombLogic drawing part WIP;

--- src/BombLogic.cpp
+++ src/BombLogic.cpp
@@ -25,6 +25,12 @@ namespace {
 	inline const unsigned char Vertical = 3;
 	inline const unsigned char HorizontalEnd = 1;
 	inline const unsigned char VerticalEnd= 4;
+
+	inline const unsigned char MiddleCore = 15;
+	inline const unsigned char HorizontalCore = 22;
+	inline const unsigned char VerticalCore = 21;
+	inline const unsigned char HorizontalEndCore = 24;
+	inline const unsigned char VerticalEndCore = 23;
 }
 
 BombLogic::BombLogic(PlayGround& playGround, const unsigned texId)
@@ -50,15 +56,29 @@ void BombLogic::Update(float) {
 bool BombLogic::Draw(int posX, int posY, float deltaTime) {
 	auto base = m_blastAnimator.ComputeSpriteId(0, deltaTime);
 
-	posX = 32;
-	posY = 4;
+	posY += 18;
+
 	m_pAtlas->Draw(posX+64, posY, base + m_blastAnimator.GetAnimOffset(HorizontalEnd));
 	m_pAtlas->Draw(posX+128, posY, base + m_blastAnimator.GetAnimOffset(Horizontal));
 	m_pAtlas->Draw(posX+192, posY, base + m_blastAnimator.GetAnimOffset(Middle));
-	m_pAtlas->Draw(posX+256, posY, base + m_blastAnimator.GetAnimOffset(Vertical));
-	m_pAtlas->Draw(posX+320, posY+2, base + m_blastAnimator.GetAnimOffset(VerticalEnd));
-	m_pAtlas->Draw(posX+320, posY+61, base + m_blastAnimator.GetAnimOffset(Vertical));
-	m_pAtlas->Draw(posX+320, posY+112, base + m_blastAnimator.GetAnimOffset(Middle));
+	m_pAtlas->Draw(posX+256, posY, base + m_blastAnimator.GetAnimOffset(VerticalEnd));
+	m_pAtlas->Draw(posX+256, posY+56, base + m_blastAnimator.GetAnimOffset(Vertical));
+	m_pAtlas->Draw(posX+256, posY+112, base + m_blastAnimator.GetAnimOffset(Vertical));
+	m_pAtlas->Draw(posX+256, posY+168, base + m_blastAnimator.GetAnimOffset(Middle));
+	m_pAtlas->Draw(posX+256, posY+224, base + m_blastAnimator.GetAnimOffset(Vertical), 180.0);
+	m_pAtlas->Draw(posX+256, posY+280, base + m_blastAnimator.GetAnimOffset(Vertical), 180.0);
+	m_pAtlas->Draw(posX+256, posY+336, base + m_blastAnimator.GetAnimOffset(VerticalEnd), 180.0);
+
+	m_pAtlas->Draw(posX+64, posY, HorizontalEndCore);
+	m_pAtlas->Draw(posX+128, posY, HorizontalCore);
+	m_pAtlas->Draw(posX+192, posY, MiddleCore);
+	m_pAtlas->Draw(posX+256, posY, VerticalEndCore);
+	m_pAtlas->Draw(posX+256, posY+56, VerticalCore);
+	m_pAtlas->Draw(posX+256, posY+112, VerticalCore);
+	m_pAtlas->Draw(posX+256, posY+168, MiddleCore);
+	m_pAtlas->Draw(posX+256, posY+224, VerticalCore, 180.0);
+	m_pAtlas->Draw(posX+256, posY+280, VerticalCore, 180.0);
+	m_pAtlas->Draw(posX+256, posY+336, VerticalEndCore, 180.0);
 
 	return true;
 }
